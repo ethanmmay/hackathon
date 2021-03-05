8f77a05724469e9462dd64efa689249042bfa801
georgecondit <geo.condit@gmail.com>
2021-03-05 16:46:20 -0700

    Page Drawing

--- client/app/Models/Post.js
+++ client/app/Models/Post.js
@@ -3,12 +3,26 @@ export default class Post {
     this.title = data.title,
     this.body = data.body || '',
     this.imgUrl = data.imgUrl || '',
-    this._id = data._id || data.id
+    this._id = data._id || data.id,
+    this.name = data.creatorId.email.split('@')[0]
   }
 
   get Template() {
     return /* html */ `
-    <div>${this.title}</div>
+    <div class="row">
+      <div class="card col-11 text-left border-radius m-5 tag-font p-5">
+          <h2>${this.title}</h2>
+          <img class="img-top" src="${this.imgUrl}">
+          <h3>${this.body}</h3>
+        
+          <div class="row ">
+            <div class="col-12 d-flex justify-content-center justify-content-between">
+              <h3>${this.name}</h3>
+              <span onclick="app.commentsController.getComment('')"><h3>COMMENTS</h3></span>
+            </div>
+          </div>
+      </div>
+    </div>
     `
   }
 }
